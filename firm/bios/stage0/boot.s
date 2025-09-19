b5aa9c248a9534e533d0e45006d9791128cc95c9
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-09-18 23:52:15 -0500

    Implement a20 line enabling

--- firm/bios/stage0/boot.s
+++ firm/bios/stage0/boot.s
@@ -1,42 +1,33 @@
+.section .boot, "awx"
+.global  _start
 .code16
 
-.global _start
-
 _start:
-	mov $0x7c00, %ax
+	xor %ax, %ax
 	mov %ax, %ds
 	mov %ax, %es
 	mov %ax, %ss
+	mov %ax, %fs
+	mov %ax, %gs
 
-	mov %ax, %sp
+	mov $0x7c00, %sp
 
-	cld
+a20:
+	in   $92, %al
+	test $2, %al
+	jnz  after_a20
+	or   $2, %al
+	and  $0xfe, %al
+	out  %al, $92
 
-	mov  $msg, %si
-	call print
+extention_check:
+
+after_a20:
 
 _end:
 	hlt
 	jmp _end
 
-print:
-	mov $0x00, %bx
-	mov $0x0e, %ah
-
-print_continue:
-	lodsb
-	cmp $0, %al
-	je  print_done
-	int $0x10
-	jmp print
-
-print_done:
-	ret
-
-msg:
-	.asciz "Hello maize"
-
 #
-.space 510 - (. - _start)
+.space 510 - ( . - _start)
 .word  0xaa55
-
