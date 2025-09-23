7d1a5b5b3361c1be13e902d0ce737a90944e9621
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-09-23 00:05:57 -0500

    Update builder.zig to new project structure

--- build/builder.zig
+++ build/builder.zig
@@ -5,21 +5,19 @@ pub const Builder = struct {
     optimize: std.builtin.OptimizeMode,
 
     pub fn buildBootloader(self: Builder, b: *std.Build) *std.Build.Step.Compile {
-        const bios_dir = b.path("firm/bios");
-
-        const stage_zero_dir = bios_dir.path(b, "stage0");
+        const first_stage_dir = b.path("stage1");
 
         const boot_mod = b.createModule(.{
             .target = self.target,
             .optimize = self.optimize,
         });
-        boot_mod.addAssemblyFile(stage_zero_dir.path(b, "boot.s"));
+        boot_mod.addAssemblyFile(first_stage_dir.path(b, "boot.S"));
 
         const boot_bin = b.addExecutable(.{
             .name = "boot.bin",
             .root_module = boot_mod,
         });
-        boot_bin.setLinkerScript(stage_zero_dir.path(b, "link.ld"));
+        boot_bin.setLinkerScript(first_stage_dir.path(b, "link.ld"));
 
         return boot_bin;
     }
