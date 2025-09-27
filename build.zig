943f5b763e5234b49059e8cda8e2bd151a896468
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-09-27 00:52:39 -0500

    Update build.zig comments

--- build.zig
+++ build.zig
@@ -4,27 +4,29 @@ const Builder = @import("utils/builder.zig").Builder;
 const qemu_util = @import("utils/qemu.zig");
 
 pub fn build(b: *std.Build) void {
+    // Options
     const arch = b.option(Architecture, "arch", "Cpu architecture (defaults to x86)") orelse Architecture.x86;
     const optimize = b.standardOptimizeOption(.{});
 
+    // Create Builder, it'll help keep the build.zig be concise
     const builder = Builder{
         .target = b.resolveTargetQuery(arch.getTargetQuery()),
         .optimize = optimize,
     };
 
-    // bootloader step
+    // Bootloader step
     const bootloader_step = b.step("bootloader", "Build the bootloader");
     const bootloader = builder.buildBootloader(b);
     const bootloader_install = b.addInstallBinFile(bootloader.getEmittedBin(), bootloader.name);
     bootloader_step.dependOn(&bootloader_install.step);
 
-    // qemu step
+    // Qemu step
     const qemu_step = b.step("qemu", "Build and run bootloader in qemu");
     qemu_step.dependOn(&bootloader.step);
     const qemu_cmd = qemu_util.createQemuCommand(b, bootloader);
     qemu_step.dependOn(&qemu_cmd.step);
 
-    // install step
+    // Install step
     const install_step = b.getInstallStep();
     install_step.dependOn(&bootloader_install.step);
 }
