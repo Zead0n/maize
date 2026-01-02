b18c5c2a28e5c89d6c9e04f0360892ea790a88d5
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-01-01 20:01:38 -0600

    Update build system to include decompress stage

--- build.zig
+++ build.zig
@@ -5,33 +5,44 @@ const qemu_util = @import("utils/qemu.zig");
 
 pub fn build(b: *std.Build) void {
     const arch = b.option(arch_util.Architecture, "arch", "Cpu architecture (defaults to x86)") orelse arch_util.Architecture.x86;
-    const optimize = b.standardOptimizeOption(.{});
+
+    const target_query = b.resolveTargetQuery(arch.getTargetQuery());
 
     const stage1 = bootloader_util.buildStage1(b, .{
-        .target = b.resolveTargetQuery(arch.getTargetQuery()),
-        .optimize = optimize,
+        .target = target_query,
+        .optimize = .ReleaseSmall,
+    });
+
+    const decompress = bootloader_util.buildDecompress(b, .{
+        .target = target_query,
+        .optimize = .ReleaseSmall,
     });
 
     const stage2 = bootloader_util.buildStageTwo(b, .{
-        .target = b.resolveTargetQuery(arch.getTargetQuery()),
-        .optimize = optimize,
+        .target = target_query,
+        .optimize = .ReleaseSmall,
     });
 
-    const bootloader = bootloader_util.buildBootloader(b, stage1, stage2);
+    const bootloader = bootloader_util.buildBootloader(b, .{
+        .first = stage1,
+        .decompress = decompress,
+        .second = stage2,
+    });
 
     // Bootloader step
     const bootloader_step = b.step("bootloader", "Build the bootloader");
     bootloader_step.dependOn(&bootloader.step);
 
     // Qemu step
-    const qemu_step = b.step("qemu", "Build and run bootloader in qemu");
-    const qemu_cmd = qemu_util.createQemuCommand(b, bootloader.source, arch.toStdArch());
-    qemu_cmd.step.dependOn(&bootloader.step);
-    qemu_step.dependOn(&qemu_cmd.step);
+    // const qemu_step = b.step("qemu", "Build and run bootloader in qemu");
+    // const qemu_cmd = qemu_util.createQemuCommand(b, bootloader.source, arch.toStdArch());
+    // qemu_cmd.step.dependOn(&bootloader.step);
+    // qemu_step.dependOn(&qemu_cmd.step);
 
     // Install step
     const install_step = b.getInstallStep();
     install_step.dependOn(&bootloader.step);
     b.installArtifact(stage1);
     b.installArtifact(stage2);
+    b.installArtifact(decompress);
 }
--- utils/bootloader.zig
+++ utils/bootloader.zig
@@ -12,7 +12,6 @@ pub fn buildStage1(b: *std.Build, options: BuildOptions) *std.Build.Step.Compile
     const first_stage_mod = b.createModule(.{
         .target = options.target,
         .optimize = options.optimize,
-        .root_source_file = first_stage_dir.path(b, "decompress.zig"),
     });
     first_stage_mod.addAssemblyFile(first_stage_dir.path(b, "boot.S"));
 
@@ -43,29 +42,64 @@ pub fn buildStageTwo(b: *std.Build, options: BuildOptions) *std.Build.Step.Compi
     return second_stage_bin;
 }
 
-pub fn buildBootloader(b: *std.Build, first_stage: *std.Build.Step.Compile, second_stage: *std.Build.Step.Compile) *std.Build.Step.InstallFile {
+pub fn buildDecompress(b: *std.Build, options: BuildOptions) *std.Build.Step.Compile {
+    const decompress_dir = b.path("src/decompress");
+
+    const decompress_mod = b.createModule(.{
+        .target = options.target,
+        .optimize = options.optimize,
+        .root_source_file = decompress_dir.path(b, "decompress.zig"),
+    });
+    decompress_mod.addAssemblyFile(decompress_dir.path(b, "entry.S"));
+
+    const decompress_bin = b.addExecutable(.{
+        .name = "decompress.bin",
+        .root_module = decompress_mod,
+    });
+    decompress_bin.setLinkerScript(decompress_dir.path(b, "decompress.ld"));
+
+    return decompress_bin;
+}
+
+const BootStages = struct {
+    first: *std.Build.Step.Compile,
+    second: *std.Build.Step.Compile,
+    decompress: *std.Build.Step.Compile,
+};
+
+pub fn buildBootloader(b: *std.Build, stages: BootStages) *std.Build.Step.InstallFile {
     const boot_files = b.addWriteFiles();
     const boot_img = boot_files.add("boot.img", "");
 
     const first_dd = dd_util.ddCmd(b, .{
         .of_lp = boot_img,
-        .if_lp = first_stage.getEmittedBin(),
+        .if_lp = stages.first.getEmittedBin(),
         .count = 1,
         .conv = &.{"notrunc"},
     });
-    first_dd.step.dependOn(&first_stage.step);
+    first_dd.step.dependOn(&stages.first.step);
+
+    const decompress_dd = dd_util.ddCmd(b, .{
+        .of_lp = boot_img,
+        .if_lp = stages.decompress.getEmittedBin(),
+        .seek = 1,
+        .count = 1,
+        .conv = &.{"notrunc"},
+    });
+    decompress_dd.step.dependOn(&stages.decompress.step);
 
     const second_dd = dd_util.ddCmd(b, .{
         .of_lp = boot_img,
-        .if_lp = second_stage.getEmittedBin(),
-        .seek = 1,
-        .count = 7,
+        .if_lp = stages.second.getEmittedBin(),
+        .seek = 2,
+        .count = 30,
         .conv = &.{"notrunc"},
     });
-    second_dd.step.dependOn(&second_stage.step);
+    second_dd.step.dependOn(&stages.second.step);
 
     const boot_img_install = b.addInstallBinFile(boot_img, "boot.img");
     boot_img_install.step.dependOn(&first_dd.step);
+    boot_img_install.step.dependOn(&decompress_dd.step);
     boot_img_install.step.dependOn(&second_dd.step);
 
     return boot_img_install;
