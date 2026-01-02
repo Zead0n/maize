f8f87b461a9a58aec40a771c545082e46dbf66ca
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-01-01 22:22:12 -0600

    Add stages build step

--- build.zig
+++ build.zig
@@ -5,8 +5,12 @@ const qemu_util = @import("utils/qemu.zig");
 
 pub fn build(b: *std.Build) void {
     const arch = b.option(arch_util.Architecture, "arch", "Cpu architecture (defaults to x86)") orelse arch_util.Architecture.x86;
-
     const target_query = b.resolveTargetQuery(arch.getTargetQuery());
+    const stages_install_dir: std.Build.Step.InstallArtifact.Options = .{
+        .dest_dir = .{
+            .override = .{ .custom = "stages" },
+        },
+    };
 
     const stage1 = bootloader_util.buildStage1(b, .{
         .target = target_query,
@@ -29,20 +33,23 @@ pub fn build(b: *std.Build) void {
         .second = stage2,
     });
 
-    // Bootloader step
-    const bootloader_step = b.step("bootloader", "Build the bootloader");
-    bootloader_step.dependOn(&bootloader.step);
+    // Stages step
+    const stages_step = b.step("stages", "Build only the stages, not the whole bootloader");
+
+    const stage1_install = b.addInstallArtifact(stage1, stages_install_dir);
+    const decompress_install = b.addInstallArtifact(decompress, stages_install_dir);
+    const stage2_install = b.addInstallArtifact(stage2, stages_install_dir);
+    stages_step.dependOn(&stage1_install.step);
+    stages_step.dependOn(&decompress_install.step);
+    stages_step.dependOn(&stage2_install.step);
 
     // Qemu step
-    // const qemu_step = b.step("qemu", "Build and run bootloader in qemu");
-    // const qemu_cmd = qemu_util.createQemuCommand(b, bootloader.source, arch.toStdArch());
-    // qemu_cmd.step.dependOn(&bootloader.step);
-    // qemu_step.dependOn(&qemu_cmd.step);
+    const qemu_step = b.step("qemu", "Build and run bootloader in qemu");
+    const qemu_cmd = qemu_util.createQemuCommand(b, bootloader.source, arch.toStdArch());
+    qemu_cmd.step.dependOn(&bootloader.step);
+    qemu_step.dependOn(&qemu_cmd.step);
 
     // Install step
     const install_step = b.getInstallStep();
     install_step.dependOn(&bootloader.step);
-    b.installArtifact(stage1);
-    b.installArtifact(stage2);
-    b.installArtifact(decompress);
 }
