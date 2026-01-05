6c776c372fbe2caedd391660d828191ecc3b712f
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-01-05 00:23:08 -0600

    Rename target variable to not be obscure

--- build/arch.zig
+++ build/arch.zig
@@ -19,10 +19,10 @@ pub const Architecture = enum {
 
         switch (self) {
             .x86 => {
-                const Target = std.Target.x86;
+                const x86_target = std.Target.x86;
 
-                query.cpu_features_add = Target.featureSet(&.{ .popcnt, .soft_float });
-                query.cpu_features_sub = Target.featureSet(&.{ .avx, .avx2, .sse, .sse2, .mmx });
+                query.cpu_features_add = x86_target.featureSet(&.{ .popcnt, .soft_float });
+                query.cpu_features_sub = x86_target.featureSet(&.{ .avx, .avx2, .sse, .sse2, .mmx });
             },
         }
 
