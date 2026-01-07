a56870b43a748f3c18d02d3ada50d8da13cdfead
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-01-06 21:51:25 -0600

    Update build to objcopy elf executables to bin

--- build.zig
+++ build.zig
@@ -6,48 +6,56 @@ const qemu_util = @import("build/commands/qemu.zig");
 pub fn build(b: *std.Build) void {
     const arch = b.option(arch_util.Architecture, "arch", "Cpu architecture (defaults to x86)") orelse arch_util.Architecture.x86;
     const target_query = b.resolveTargetQuery(arch.getTargetQuery());
+
+    const stages_dir: std.Build.InstallDir = .{ .custom = "stages" };
     const stages_install_dir: std.Build.Step.InstallArtifact.Options = .{
         .dest_dir = .{
-            .override = .{ .custom = "stages" },
+            .override = stages_dir,
         },
     };
 
-    const stage1 = bootloader_util.buildStage1(b, .{
+    const stage1_elf = bootloader_util.buildStage1(b, .{
         .target = target_query,
         .optimize = .ReleaseSmall,
     });
+    const stage1_bin = b.addObjCopy(stage1_elf.getEmittedBin(), .{ .format = .bin });
+    stage1_bin.step.dependOn(&stage1_elf.step);
 
-    const true_stage1 = b.addObjCopy(stage1.getEmittedBin(), .{
-        .format = .bin,
-    });
-
-    const stage2 = bootloader_util.buildStageTwo(b, .{
+    const stage2_elf = bootloader_util.buildStageTwo(b, .{
         .target = target_query,
         .optimize = .ReleaseSmall,
     });
+    const stage2_bin = b.addObjCopy(stage1_elf.getEmittedBin(), .{ .format = .bin });
+    stage2_bin.step.dependOn(&stage2_elf.step);
 
-    const decompress = bootloader_util.buildDecompress(b, stage2, .{
+    const decompress_elf = bootloader_util.buildDecompress(b, stage2_bin.getOutput(), .{
         .target = target_query,
         .optimize = .ReleaseSmall,
     });
+    const decompress_bin = b.addObjCopy(stage1_elf.getEmittedBin(), .{ .format = .bin });
+    decompress_bin.step.dependOn(&decompress_elf.step);
 
     const bootloader = bootloader_util.buildBootloader(b, .{
-        .first = stage1,
-        .decompress = decompress,
-        .second = stage2,
+        .first = stage1_bin.getOutput(),
+        .decompress = decompress_bin.getOutput(),
+        .second = stage2_bin.getOutput(),
     });
 
     // Stages step
     const stages_step = b.step("stages", "Build only the stages, not the whole bootloader");
 
-    const stage1_install = b.addInstallArtifact(stage1, stages_install_dir);
-    const true_stage1_install = b.addInstallFileWithDir(true_stage1.getOutput(), .{ .custom = "stages" }, "true_stage1.bin");
-    const decompress_install = b.addInstallArtifact(decompress, stages_install_dir);
-    const stage2_install = b.addInstallArtifact(stage2, stages_install_dir);
-    stages_step.dependOn(&stage1_install.step);
-    stages_step.dependOn(&true_stage1_install.step);
-    stages_step.dependOn(&decompress_install.step);
-    stages_step.dependOn(&stage2_install.step);
+    const stage1_elf_install = b.addInstallArtifact(stage1_elf, stages_install_dir);
+    const stage2_elf_install = b.addInstallArtifact(stage2_elf, stages_install_dir);
+    const decompress_elf_install = b.addInstallArtifact(decompress_elf, stages_install_dir);
+    const stage1_bin_install = b.addInstallFileWithDir(stage1_bin.getOutput(), stages_dir, "stage1.bin");
+    const stage2_bin_install = b.addInstallFileWithDir(stage2_bin.getOutput(), stages_dir, "stage2.bin");
+    const decompress_bin_install = b.addInstallFileWithDir(decompress_bin.getOutput(), stages_dir, "decompress.bin");
+    stages_step.dependOn(&stage1_elf_install.step);
+    stages_step.dependOn(&stage2_elf_install.step);
+    stages_step.dependOn(&decompress_elf_install.step);
+    stages_step.dependOn(&stage1_bin_install.step);
+    stages_step.dependOn(&stage2_bin_install.step);
+    stages_step.dependOn(&decompress_bin_install.step);
 
     // Qemu step
     const qemu_step = b.step("qemu", "Build and run bootloader in qemu");
