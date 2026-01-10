bbbcfb6e3ed83079bab117cfd604c04c10b66c16
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-01-10 14:25:43 -0600

    Update decompressor stage

--- src/decompress/decompress.zig
+++ src/decompress/decompress.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
-const data = @embedFile("stage2");
+const stage2_data = @embedFile("stage2");
 
 export fn decompress() noreturn {
-    var reader = std.Io.Reader.fixed(data);
+    var reader = std.Io.Reader.fixed(stage2_data);
     const stage2_addr: usize = 0xc000;
     var dest: [*]u8 = @ptrFromInt(stage2_addr);
 
@@ -12,7 +12,9 @@ export fn decompress() noreturn {
     } else |_| {}
 
     asm volatile (
-        \\jmp 0xc000
+        \\jmp %[stage2:P]
+        :
+        : [stage2] "X" (stage2_addr),
     );
 
     while (true)
@@ -25,23 +27,23 @@ export fn decompress() noreturn {
 // so the uncompressed version of stage2 needs to be embedded and loaded.
 // Maybe this will work in the future.
 //
-// export fn broken_decompress() linksection(".text") noreturn {
-//     var decom_buf: [std.compress.flate.max_window_len]u8 = undefined;
-//     var compressed_reader = std.Io.Reader.fixed(data);
-//
-//     const decompressed = std.compress.flate.Decompress.init(&compressed_reader, .gzip, &decom_buf);
+// export fn decompress() noreturn {
+//     var compressed_reader = std.Io.Reader.fixed(stage2_data);
+//     const decompressed = std.compress.flate.Decompress.init(&compressed_reader, .gzip, &.{});
 //     var reader: *std.Io.Reader = @constCast(&decompressed.reader);
 //
 //     const stage2_addr: usize = 0xc000;
 //     var dest: [*]u8 = @ptrFromInt(stage2_addr);
 //
 //     var i: usize = 0;
-//     while (reader.takeByte()) |byte| : (i += 1) {
-//         dest[i] = byte;
+//     while (reader.readSliceShort(dest[i * 512 .. (i + 1) * 512])) |read_size| : (i += 1) {
+//         if (read_size < 512) break;
 //     } else |_| {}
 //
 //     asm volatile (
-//         \\jmp 0xc000
+//         \\jmp %[stage2:P]
+//         :
+//         : [stage2] "X" (stage2_addr),
 //     );
 //
 //     while (true)
