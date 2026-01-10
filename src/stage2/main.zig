af80af18b03a8c4da829b247c9c965a14b329c6d
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-01-10 07:17:28 -0600

    Implement bare bones stage2

--- src/stage2/main.zig
+++ src/stage2/main.zig
@@ -1,3 +1,12 @@
 export fn _start() callconv(.naked) noreturn {
-    asm volatile ("hlt");
+    asm volatile (
+        \\movw %%ax, (0xb8000)
+        :
+        : [char] "{ax}" (0x0f5a),
+    );
+
+    while (true)
+        asm volatile ("hlt");
+
+    unreachable;
 }
