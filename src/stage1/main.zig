2ff51edd2b68fd9e7dcdd57c8dc481fade0aaea8
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-02-03 13:49:43 -0600

    Use disk address packet with segment over offset

--- src/stage1/main.zig
+++ src/stage1/main.zig
@@ -4,11 +4,11 @@ const utils = @import("utils");
 const teletype = utils.teletype;
 
 const STAGE_TWO_DEST = 0x8000;
-const DAP: dap.DiskAddressPacket = .{
+const DAP_ENTRY: dap.DiskAddressPacket = .{
     .lba = 1,
     .blocks = 7,
-    .offset = STAGE_TWO_DEST,
-    .segment = 0,
+    .offset = 0,
+    .segment = (STAGE_TWO_DEST >> 4),
 };
 
 export var drive: u8 = 0;
@@ -16,14 +16,14 @@ export var drive: u8 = 0;
 export fn first_stage() noreturn {
     if (!dap.check_ext13(drive)) @panic("!E");
     // dap_entry.read(drive) catch @panic("!R");
-    if (DAP.read(drive)) |err_code| {
+    if (DAP_ENTRY.read(drive)) |err_code| {
         teletype.put(err_code);
         @panic("!R");
     }
 
     asm volatile ("jmp %[stage2_addr:a]"
         :
-        : [stage2_addr] "X" (STAGE_TWO_DEST),
+        : [stage2_addr] "i" (STAGE_TWO_DEST),
     );
 
     asm volatile ("hlt");
