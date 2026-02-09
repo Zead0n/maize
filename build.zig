fc5977ccfcc247ada7dee982e6912163aa52ffa7
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-02-09 10:48:09 -0600

    Delete commented build code

--- build.zig
+++ build.zig
@@ -8,31 +8,6 @@ pub fn build(b: *std.Build) void {
     const arch = b.option(arch_util.Architecture, "arch", "Cpu architecture (defaults to x86)") orelse .x86;
 
     buildBios(b, arch);
-
-    // const bios_stages = bootloader_util.buildBiosStages(b, arch);
-    // const bios_bootloader = bootloader_util.buildBiosBootloader(
-    //     b,
-    //     b.fmt("maize-bios-{s}.img", .{@tagName(arch)}),
-    //     bios_stages,
-    // );
-
-    // // Bios step
-    // const bios_step = b.step("bios", "Build bios");
-    // bios_step.dependOn(&bios_bootloader.step);
-    //
-    // // Bios disect step
-    // const bios_disect_step = b.step("bios-disect", "Disect bios stages");
-    // const bios_install_dir = std.Build.InstallDir{ .custom = "bios" };
-    // const stage1_elf_install = b.addInstallFileWithDir(bios_stages.stage_one.getEmittedBin(), bios_install_dir, "stage1.elf");
-    // const stage2_elf_install = b.addInstallFileWithDir(bios_stages.stage_two.getEmittedBin(), bios_install_dir, "stage2.elf");
-    // bios_disect_step.dependOn(&stage1_elf_install.step);
-    // bios_disect_step.dependOn(&stage2_elf_install.step);
-    //
-    // // Bios qemu step
-    // const bios_qemu_step = b.step("bios-qemu", "Build bios and run qemu");
-    // const bios_qemu_cmd = qemu_util.createQemuCommand(b, bios_bootloader.source, arch.toStdArch());
-    // bios_qemu_cmd.step.dependOn(&bios_bootloader.step);
-    // bios_qemu_step.dependOn(&bios_qemu_cmd.step);
 }
 
 fn buildBios(b: *std.Build, arch: arch_util.Architecture) void {
