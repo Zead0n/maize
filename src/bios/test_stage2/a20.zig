b624c77a09921818a9205d75236eb53943ff69dd
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-02-25 19:20:11 -0600

    Fix cpu import to sys

--- src/bios/test_stage2/a20.zig
+++ src/bios/test_stage2/a20.zig
@@ -1,4 +1,4 @@
-const cpu = @import("cpu.zig");
+const sys = @import("sys.zig");
 const real = @import("real.zig");
 
 pub fn enable() error{FailedA20}!void {
@@ -11,26 +11,26 @@ pub fn enable() error{FailedA20}!void {
 
     kbc_enable: {
         if (!kbcWait(2, 0)) break :kbc_enable;
-        cpu.out(u8, 0x64, 0xad);
+        sys.out(u8, 0x64, 0xad);
         if (!kbcWait(2, 0)) break :kbc_enable;
-        cpu.out(u8, 0x64, 0xd0);
+        sys.out(u8, 0x64, 0xd0);
         if (!kbcWait(1, 1)) break :kbc_enable;
-        const kbc_byte = cpu.in(u8, 0x60);
+        const kbc_byte = sys.in(u8, 0x60);
         if (!kbcWait(2, 0)) break :kbc_enable;
-        cpu.out(u8, 0x64, 0xd1);
+        sys.out(u8, 0x64, 0xd1);
         if (!kbcWait(2, 0)) break :kbc_enable;
-        cpu.out(u8, 0x64, kbc_byte | 2);
+        sys.out(u8, 0x64, kbc_byte | 2);
         if (!kbcWait(2, 0)) break :kbc_enable;
-        cpu.out(u8, 0x64, 0xae);
+        sys.out(u8, 0x64, 0xae);
     }
 
     if (check()) return;
 
-    var fast_byte = cpu.in(u8, 0x92);
+    var fast_byte = sys.in(u8, 0x92);
     if ((fast_byte & 0x02) == 0) {
         fast_byte |= 0x02;
         fast_byte &= 0xfe;
-        cpu.out(u8, 0x92, fast_byte);
+        sys.out(u8, 0x92, fast_byte);
     }
 
     if (check()) return;
@@ -42,15 +42,15 @@ fn check() bool {
     const boot_id_addr = 0x7dfe;
     const compare_addr = boot_id_addr + 0x100000;
 
-    const original = cpu.memIn(u16, boot_id_addr);
-    defer cpu.memOut(u16, boot_id_addr, original);
+    const original = sys.memIn(u16, boot_id_addr);
+    defer sys.memOut(u16, boot_id_addr, original);
 
-    cpu.memOut(u16, boot_id_addr, 0x1234);
-    if (cpu.memIn(u16, boot_id_addr) != cpu.memIn(u16, compare_addr))
+    sys.memOut(u16, boot_id_addr, 0x1234);
+    if (sys.memIn(u16, boot_id_addr) != sys.memIn(u16, compare_addr))
         return true;
 
-    cpu.memOut(u16, boot_id_addr, ~cpu.memIn(u16, boot_id_addr));
-    if (cpu.memIn(u16, boot_id_addr) != cpu.memIn(u16, compare_addr))
+    sys.memOut(u16, boot_id_addr, ~sys.memIn(u16, boot_id_addr));
+    if (sys.memIn(u16, boot_id_addr) != sys.memIn(u16, compare_addr))
         return true;
 
     return false;
@@ -59,7 +59,7 @@ fn check() bool {
 fn kbcWait(mask: u8, expected: u8) bool {
     const timeout = 50000;
     for (0..timeout) |_| {
-        if ((cpu.in(u8, 0x64) & mask) == expected)
+        if ((sys.in(u8, 0x64) & mask) == expected)
             return true;
     }
 
