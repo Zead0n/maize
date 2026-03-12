cb553f919d947dd53506d6ca03f69bfffe6e8641
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-03-12 14:02:53 -0500

    Remove lldb from flake packages

--- flake.nix
+++ flake.nix
@@ -12,8 +12,7 @@
         (system: f { pkgs = import inputs.nixpkgs { inherit system; }; });
     in {
       devShells = forEachSupportedSystem ({ pkgs }: {
-        default =
-          pkgs.mkShell { packages = with pkgs; [ zig zls lldb qemu gdb ]; };
+        default = pkgs.mkShell { packages = with pkgs; [ zig zls qemu gdb ]; };
       });
     };
 }
