7920a2987423381748eb8250c4a47429c1096659
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-07-15 05:09:43 -0500

    Delete comments

--- core/firm.zig
+++ core/firm.zig
@@ -1,18 +1,5 @@
 const std = @import("std");
 
-// pub const FirmVtable = struct {
-//     init: *const fn (*anyopaque) anyerror!void,
-//     setResolution: *const fn (*anyopaque) anyerror!void,
-// };
-
 pub const Firm = struct {
     init: *const fn () anyerror!void,
-    // pub fn run(self: @This()) !void {
-    //     self.vtable.init(self.ptr) catch |e| {
-    //         std.log.err("Initialization failed - {s}", .{@errorName(e)});
-    //         return error.Init;
-    //     };
-    //
-    //     self.vtable.setResolution(self.ptr) catch |e| std.log.warn("Failed setting resolution - {s}", .{@errorName(e)});
-    // }
 };
--- core/main.zig
+++ core/main.zig
@@ -9,17 +9,3 @@ pub fn run(os_firm: *const Firm) !void {
         return error.Init;
     };
 }
-
-// pub const BootFirm = struct {
-//     init: *const fn () anyerror!void,
-//     setResolution: *const fn () anyerror!void,
-//
-//     pub fn run(self: @This()) !void {
-//         self.init() catch |e| {
-//             std.log.err("Initialization failed - {s}", .{@errorName(e)});
-//             return error.Init;
-//         };
-//
-//         self.setResolution() catch |e| std.log.warn("Failed setting resolution - {s}", .{@errorName(e)});
-//     }
-// };
