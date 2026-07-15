af9466109d06de477e23a91ce7df98e23b839ed8
Zead0n <141008297+Zead0n@users.noreply.github.com>
2026-07-15 14:02:12 -0500

    Cleanup bios stage1

--- firm/bios/stage1.zig
+++ firm/bios/stage1.zig
@@ -1,71 +1,24 @@
 const std = @import("std");
-
-pub const DiskAddressPacket = packed struct {
-    size: u8 = 0x10,
-    reserved: u8 = 0,
-    blocks: u16,
-    offset: u16,
-    segment: u16,
-    lba: u64,
-
-    pub fn read(self: @This(), disk: u8) error{ NotSupported, ReadFailed }!void {
-        const check_value: u16 = asm (
-            \\int $0x13
-            : [ret] "={bx}" (-> u16),
-            : [func] "{ax}" (0x4100),
-              [magic] "{bx}" (0x55aa),
-              [drive] "{dx}" (disk),
-        );
-
-        if (check_value != 0xaa55)
-            return error.NotSupported;
-
-        const result: u16 = asm (
-            \\int $0x13
-            : [ret] "={ax}" (-> u16),
-            : [ax] "{ax}" (0x4200),
-              [disk_num] "{dx}" (disk),
-              [dap_addr] "{si}" (&self),
-        );
-
-        if (@as(u8, @truncate(result)) != 0)
-            return error.ReadFailed;
-    }
-};
+const disk = @import("common/disk.zig");
 
 const STAGE2_DEST = 0xa000;
-const DAP: DiskAddressPacket = .{
+const DAP: disk.DiskAddressPacket = .{
     .lba = 1,
     .blocks = 63,
     .offset = 0,
     .segment = (STAGE2_DEST >> 4),
 };
 
-export fn firstStage() noreturn {
-    const drive: u8 = asm (
-        \\movb %%dl, %[ret]
-        : [ret] "=r" (-> u8),
-    );
+pub const panic = std.debug.FullPanic(rmPanic);
+fn rmPanic(msg: []const u8, _: ?usize) noreturn {
+    put('!');
+    for (msg) |char|
+        put(char);
 
-    DAP.read(drive) catch |err| switch (err) {
-        error.NotSupported => @panic("E"),
-        error.ReadFailed => @panic("R"),
-    };
-
-    asm volatile (
-        \\push %[disk]
-        \\jmp %[stage2:a]
-        :
-        : [disk] "{dx}" (@as(u16, drive)),
-          [stage2] "i" (STAGE2_DEST),
-    );
+    while (true)
+        asm volatile ("hlt");
 
     unreachable;
-}
-
-fn puts(chars: []const u8) void {
-    for (chars) |char|
-        put(char);
 }
 
 fn put(char: u8) void {
@@ -76,13 +29,24 @@ fn put(char: u8) void {
     );
 }
 
-pub const panic = std.debug.FullPanic(rmPanic);
-fn rmPanic(msg: []const u8, _: ?usize) noreturn {
-    put('!');
-    puts(msg);
+export fn firstStage() noreturn {
+    const drive: u8 = asm (
+        \\movb %%dl, %[ret]
+        : [ret] "=r" (-> u8),
+    );
 
-    while (true)
-        asm volatile ("hlt");
+    DAP.read(drive) catch |err| switch (err) {
+        disk.DapError.NotSupported => @panic("E"),
+        disk.DapError.ReadFailed => @panic("R"),
+    };
+
+    asm volatile (
+        \\push %[disk]
+        \\jmp %[stage2:a]
+        :
+        : [disk] "{dx}" (@as(u16, drive)),
+          [stage2] "i" (STAGE2_DEST),
+    );
 
     unreachable;
 }
